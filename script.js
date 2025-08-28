6e6be29446d890b439058a02be9b7176074f6e53
Obilana Eniola <obilanaeniola01@gmail.com>
2025-08-28 20:36:27 +0100

    fikayo js

---
+++ script.js
@@ -0,0 +1,61 @@
+/*
+    var name = "fikayo";
+    console.log(name);
+
+    name = "tise";
+
+    console.log(name);
+
+
+    const name = "fikayo";
+    console.log(name);
+
+    name = "tise";
+    console.log(name);
+
+
+
+
+
+    alert("hi , welcome to my site");
+
+    prompt("how are you today ?");
+
+
+    let milo = 2000;
+    let sardine = 200;
+    let egg = 300;
+    let kopiko = 20;
+
+    let name = prompt("whats your name ?");
+    alert("welcome to my shop " + name);
+    let moneyInPocket = prompt("how much do you have ?");
+
+    let miloTotal = milo * prompt("how many milo do you want to buy ?");
+    console.log(miloTotal);
+
+    let sardineTotal = sardine * prompt("how many sardine do you want to buy ?");
+    console.log(sardineTotal);
+
+    let eggTotal = egg * prompt("how many egg do you want to buy ?");
+    console.log(eggTotal);
+
+    let kopikoTotal = kopiko * prompt("how many kopiko do you want to buy ?");
+    console.log(kopikoTotal);
+
+    let totalPrice = miloTotal + sardineTotal + eggTotal + kopikoTotal;
+    console.log(totalPrice);
+
+    let change = moneyInPocket - totalPrice;
+    console.log(change);
+
+    alert(name + " your total bill is " + totalPrice);
+    alert(name + " your change is " + change);
+
+    alert("thanks for your patronage");
+    */
+
+console.log(2 > 1);
+console.log(2 < 1);
+console.log(2 == "2");
+console.log(2 === "2");
